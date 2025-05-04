56d246580a595ef3030c3583495e42f34b3d20f0
Lucas <lucas.iferreir@gmail.com>
2025-05-04 00:33:55 -0300

    Atualização de usuário com PUT

--- src/main/java/com/lucasferreira/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasferreira/workshopmongo/services/UserService.java
@@ -32,8 +32,19 @@ public class UserService {
     public void delete(String id) {
         findById(id);
         repository.deleteById(id);
-
     }
+
+    public User update (User obj){
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repository.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj){
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
 
     public User fromDTO(UserDTO userDTO) {
         return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
