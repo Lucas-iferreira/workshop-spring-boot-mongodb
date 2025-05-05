d1571de29336ac601e248220bd622a1e137c6774
Lucas <lucas.iferreir@gmail.com>
2025-05-05 08:24:06 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/lucasferreira/workshopmongo/domain/Post.java
+++ src/main/java/com/lucasferreira/workshopmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.lucasferreira.workshopmongo.domain;
 
+import com.lucasferreira.workshopmongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -18,12 +19,12 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -31,11 +32,11 @@ public class Post implements Serializable {
         this.author = author;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
