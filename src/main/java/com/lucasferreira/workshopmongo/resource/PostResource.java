9a45a0619b06d23c51594214b852a0ccabb906d1
Lucas <lucas.iferreir@gmail.com>
2025-05-07 11:15:35 -0300

    Consulta com vários critérios

--- src/main/java/com/lucasferreira/workshopmongo/resource/PostResource.java
+++ src/main/java/com/lucasferreira/workshopmongo/resource/PostResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -19,16 +20,26 @@ public class PostResource {
     private PostService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Post> findById(@PathVariable String id){
+    public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
     @GetMapping(value = "/titlesearch")
-    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text" , defaultValue = "") String text){
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
         return ResponseEntity.ok().body(list);
     }
 
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSeacrh(@RequestParam(value = "text", defaultValue = "") String text,
+                                                 @RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                                 @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.converDate(minDate, new Date(0L));
+        Date max = URL.converDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+    }
 }
--- src/main/java/com/lucasferreira/workshopmongo/services/PostService.java
+++ src/main/java/com/lucasferreira/workshopmongo/services/PostService.java
@@ -9,6 +9,7 @@ import com.lucasferreira.workshopmongo.services.exception.ObjectNotFoundExceptio
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,7 +24,12 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
-    public List<Post> findByTitle(String text){
-       return repository.searchTitle(text);
+    public List<Post> findByTitle(String text) {
+        return repository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
