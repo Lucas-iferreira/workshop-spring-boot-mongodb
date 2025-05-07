84622e6ec72d46bca8b041225580e089e20f2d8f
Lucas <lucas.iferreir@gmail.com>
2025-05-07 10:17:15 -0300

    Consulta simples com query methods

--- src/main/java/com/lucasferreira/workshopmongo/resource/PostResource.java
+++ src/main/java/com/lucasferreira/workshopmongo/resource/PostResource.java
@@ -1,10 +1,14 @@
 package com.lucasferreira.workshopmongo.resource;
 
 import com.lucasferreira.workshopmongo.domain.Post;
+import com.lucasferreira.workshopmongo.resource.util.URL;
 import com.lucasferreira.workshopmongo.services.PostService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -18,6 +22,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text" , defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
