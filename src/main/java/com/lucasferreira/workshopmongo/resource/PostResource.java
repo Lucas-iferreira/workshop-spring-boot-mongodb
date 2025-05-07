7e4416281f03a6fe23280282799c2e3b82efc2cb
Lucas <lucas.iferreir@gmail.com>
2025-05-07 09:13:42 -0300

    Obtendo um post por id

--- src/main/java/com/lucasferreira/workshopmongo/resource/PostResource.java
+++ src/main/java/com/lucasferreira/workshopmongo/resource/PostResource.java
@@ -1,17 +1,11 @@
 package com.lucasferreira.workshopmongo.resource;
 
 import com.lucasferreira.workshopmongo.domain.Post;
-import com.lucasferreira.workshopmongo.domain.User;
-import com.lucasferreira.workshopmongo.dto.UserDTO;
 import com.lucasferreira.workshopmongo.services.PostService;
-import com.lucasferreira.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
